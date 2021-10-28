b4fcb0d9bbc2ededeaa2fc84ef6bff406882a970
selenium22 <93306330+selenium22@users.noreply.github.com>
2021-10-28 10:40:08 +0530

    Initial commit

---
+++ Sample.java
@@ -0,0 +1,17 @@
+class Sample
+{
+  public static void main(String args[])
+  {
+   System.out.println("Welcome To Java");
+   byte a=10;
+   byte b=10; 
+   int c=a+b;
+
+   //float b=34.67f;
+   //float f=a;
+   //System.out.println(a);
+   System.out.println(c);
+   //System.out.println(f);
+   
+  }
+}
